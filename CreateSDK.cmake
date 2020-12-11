93e7b5798d0ca8668e3712ec8295a5f26daf2c18
Jonatino <jonathanbeaudoin1996@gmail.com>
2020-12-11 01:00:46 -0500

    Export bmalloc for linux and mac builds

--- CreateSDK.cmake
+++ CreateSDK.cmake
@@ -1,15 +1,23 @@
 INSTALL(TARGETS WebCore
-  RUNTIME DESTINATION "bin"
-  LIBRARY DESTINATION "bin"
-  ARCHIVE DESTINATION "lib"
-)
+        RUNTIME DESTINATION "bin"
+        LIBRARY DESTINATION "bin"
+        ARCHIVE DESTINATION "lib"
+        )
 
 if (STATIC_BUILD)
-  INSTALL(TARGETS WTF JavaScriptCore PAL sqlite3
-    RUNTIME DESTINATION "bin"
-    LIBRARY DESTINATION "bin"
-    ARCHIVE DESTINATION "lib"
-  )
+    if (PORT MATCHES "UltralightWin")
+        INSTALL(TARGETS WTF JavaScriptCore PAL sqlite3
+                RUNTIME DESTINATION "bin"
+                LIBRARY DESTINATION "bin"
+                ARCHIVE DESTINATION "lib"
+                )
+    else ()
+        INSTALL(TARGETS WTF JavaScriptCore PAL sqlite3 bmalloc
+                RUNTIME DESTINATION "bin"
+                LIBRARY DESTINATION "bin"
+                ARCHIVE DESTINATION "lib"
+                )
+    endif ()
 endif ()
 
 INSTALL(DIRECTORY "${DERIVED_SOURCES_DIR}" DESTINATION "." FILES_MATCHING PATTERN "*.h" PATTERN "*.js")
@@ -35,17 +43,17 @@ INSTALL(FILES "${PROJECT_SOURCE_DIR}/Source/WebCore/config.h"
         DESTINATION "DerivedSources/ForwardingHeaders/WebCore/")
 
 set(JAVASCRIPTCORE_HEADERS
-  "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JavaScript.h"
-  "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSBase.h"
-  "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSContextRef.h"
-  "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSStringRef.h"
-  "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSObjectRef.h"
-  "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSTypedArray.h"
-  "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSValueRef.h"
-  "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/WebKitAvailability.h"
-  "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSObjectRefPrivate.h"
-  "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSRetainPtr.h"
-  )
+        "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JavaScript.h"
+        "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSBase.h"
+        "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSContextRef.h"
+        "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSStringRef.h"
+        "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSObjectRef.h"
+        "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSTypedArray.h"
+        "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSValueRef.h"
+        "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/WebKitAvailability.h"
+        "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSObjectRefPrivate.h"
+        "${PROJECT_SOURCE_DIR}/Source/JavaScriptCore/API/JSRetainPtr.h"
+        )
 
 INSTALL(FILES ${JAVASCRIPTCORE_HEADERS} DESTINATION "include/JavaScriptCore/")
 
@@ -55,14 +63,14 @@ set(WEBINSPECTORUI_DIR "${CMAKE_SOURCE_DIR}/Source/WebInspectorUI")
 
 INSTALL(DIRECTORY "${WEBINSPECTORUI_DIR}/UserInterface/" DESTINATION "inspector")
 INSTALL(FILES "${WEBINSPECTORUI_DIR}/Localizations/en.lproj/localizedStrings.js"
-              DESTINATION "inspector")
+        DESTINATION "inspector")
 INSTALL(FILES "${DERIVED_SOURCES_JAVASCRIPTCORE_DIR}/inspector/InspectorBackendCommands.js"
-              DESTINATION "inspector/Protocol")
+        DESTINATION "inspector/Protocol")
 
 if (PORT MATCHES "UltralightWin")
-  if (${CMAKE_BUILD_TYPE} MATCHES Debug OR ${CMAKE_BUILD_TYPE} MATCHES RelWithDebInfo)
-    INSTALL(FILES "${PROJECT_BINARY_DIR}/bin/WebCore.pdb" DESTINATION "bin")
-  endif ()
+    if (${CMAKE_BUILD_TYPE} MATCHES Debug OR ${CMAKE_BUILD_TYPE} MATCHES RelWithDebInfo)
+        INSTALL(FILES "${PROJECT_BINARY_DIR}/bin/WebCore.pdb" DESTINATION "bin")
+    endif ()
 endif ()
 
 if (CMAKE_SIZEOF_VOID_P EQUAL 8)
@@ -88,70 +96,70 @@ elseif (PORT MATCHES "UltralightWin")
 endif ()
 
 set(INSTALL_DIR ${PROJECT_BINARY_DIR}/out)
-    
-add_custom_target(create_sdk ALL "${CMAKE_COMMAND}" 
-    -D CMAKE_INSTALL_PREFIX:string=${INSTALL_DIR}
-    -P "${CMAKE_CURRENT_BINARY_DIR}/cmake_install.cmake" 
-    DEPENDS WebCore) 
-    
+
+add_custom_target(create_sdk ALL "${CMAKE_COMMAND}"
+        -D CMAKE_INSTALL_PREFIX:string=${INSTALL_DIR}
+        -P "${CMAKE_CURRENT_BINARY_DIR}/cmake_install.cmake"
+        DEPENDS WebCore)
+
 # Get name of current branch
 execute_process(
-  COMMAND git rev-parse --abbrev-ref HEAD
-  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-  OUTPUT_VARIABLE GIT_BRANCH
-  OUTPUT_STRIP_TRAILING_WHITESPACE
+        COMMAND git rev-parse --abbrev-ref HEAD
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_BRANCH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
 # Get status of current branch
 execute_process(
-  COMMAND git status --untracked-files=no --porcelain
-  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-  OUTPUT_VARIABLE GIT_STATUS
-  OUTPUT_STRIP_TRAILING_WHITESPACE
+        COMMAND git status --untracked-files=no --porcelain
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_STATUS
+        OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
 # Get abbreviated commit hash of current branch
 execute_process(
-  COMMAND git rev-parse --short=8 HEAD
-  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-  OUTPUT_VARIABLE GIT_COMMIT_HASH
-  OUTPUT_STRIP_TRAILING_WHITESPACE
+        COMMAND git rev-parse --short=8 HEAD
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_COMMIT_HASH
+        OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
 # Get list of local, unpushed commits
 execute_process(
-  COMMAND git cherry -v
-  WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
-  OUTPUT_VARIABLE GIT_CHERRY
-  OUTPUT_STRIP_TRAILING_WHITESPACE
+        COMMAND git cherry -v
+        WORKING_DIRECTORY ${PROJECT_SOURCE_DIR}
+        OUTPUT_VARIABLE GIT_CHERRY
+        OUTPUT_STRIP_TRAILING_WHITESPACE
 )
 
 if (APPLE)
-  if (NOT ${BUILD_DBG})
-    # Strip non-essential symbols from dylib in release build on macOS
-    add_custom_command(TARGET create_sdk POST_BUILD
-        COMMAND strip -u -r bin/libWebCore.dylib
-        WORKING_DIRECTORY ${INSTALL_DIR}
-    )
-  endif ()
+    if (NOT ${BUILD_DBG})
+        # Strip non-essential symbols from dylib in release build on macOS
+        add_custom_command(TARGET create_sdk POST_BUILD
+                COMMAND strip -u -r bin/libWebCore.dylib
+                WORKING_DIRECTORY ${INSTALL_DIR}
+                )
+    endif ()
 endif ()
 
 set(PKG_FILENAME "webcore-bin-${GIT_COMMIT_HASH}-${PLATFORM}-${ARCHITECTURE}.7z")
 
 if (NOT GIT_STATUS STREQUAL "")
     add_custom_command(TARGET create_sdk POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E echo "NOTE: No release archive created, working directory has been modified."
-    )
+            COMMAND ${CMAKE_COMMAND} -E echo "NOTE: No release archive created, working directory has been modified."
+            )
 elseif (NOT GIT_CHERRY STREQUAL "")
     add_custom_command(TARGET create_sdk POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E echo "NOTE: No release archive created, branch needs to be pushed to remote repository."
-    )
+            COMMAND ${CMAKE_COMMAND} -E echo "NOTE: No release archive created, branch needs to be pushed to remote repository."
+            )
 else ()
     add_custom_command(TARGET create_sdk POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E tar "cf" ${PROJECT_BINARY_DIR}/${PKG_FILENAME} --format=7zip -- .
-        WORKING_DIRECTORY ${INSTALL_DIR}
-    )
+            COMMAND ${CMAKE_COMMAND} -E tar "cf" ${PROJECT_BINARY_DIR}/${PKG_FILENAME} --format=7zip -- .
+            WORKING_DIRECTORY ${INSTALL_DIR}
+            )
     add_custom_command(TARGET create_sdk POST_BUILD
-        COMMAND ${CMAKE_COMMAND} -E echo "Created release archive: ${PROJECT_BINARY_DIR}/${PKG_FILENAME}"
-    )
+            COMMAND ${CMAKE_COMMAND} -E echo "Created release archive: ${PROJECT_BINARY_DIR}/${PKG_FILENAME}"
+            )
 endif ()
